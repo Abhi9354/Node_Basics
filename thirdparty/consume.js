24136b1c2f288b1941d0395a6282eab7467cad83
knowledge <abhi7ty4@gmail.com>
2024-04-08 21:38:40 +0530

    sad

--- thirdparty/consume.js
+++
@@ -1,9 +0,0 @@
-const figlet = require('figlet');
-figlet('Brain Mentors',(err, result)=>{
-    if(err){
-        console.log('Problem Using Figlet ', err);
-    }
-    else{
-        console.log(result);
-    }
-})
